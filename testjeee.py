04ea5e59c38d56b2df49a1f1b8d987b95e134fd7
Richard Jansen <richard@lrtbl.nl>
2015-06-22 17:26:49 +0200

    Deleted
    No longer needed

--- testjeee.py
+++
@@ -1,8 +0,0 @@
-Lysanne = input(str("Kent Lysanne github een beetje?"))
-
-if Lysanne == ("ja") or ("Ja") or ("j") or ("J"):
-    print ("Mooizo lysanne, mega trots op je lizzie!!")
-    
-else:
-    print ("RQ life, YOLO")
-    
